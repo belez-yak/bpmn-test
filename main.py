6a3d3f11902e3e273f90463d93b1a864028a9f85
belez-yak <belezyak.u@gmail.com>
2026-04-01 23:32:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+﻿from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_index():
+    return FileResponse("static/index.html")
+
+@app.get("/diagram")
+def get_diagram():
+    return FileResponse("diagrams/sample.bpmn", media_type="application/xml")
