320fc94755bf2623c386fb6ef47b7bd27f032eed
belez-yak <belezyak.u@gmail.com>
2026-04-02 03:44:44 +0300

    README + small changes

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-﻿from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
+DIAGRAM_PATH = BASE_DIR / "diagrams" / "sample.bpmn"
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
 
 @app.get("/")
 def read_index():
-    return FileResponse("static/index.html")
+    return FileResponse(STATIC_DIR / "index.html")
+
 
 @app.get("/diagram")
 def get_diagram():
-    return FileResponse("diagrams/sample.bpmn", media_type="application/xml")
+    return FileResponse(DIAGRAM_PATH, media_type="application/xml")
